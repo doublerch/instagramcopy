d04e2cf94b2f3f3f02e73d185e628eea2314d1c9
Ryan Chen <chencnk@bc.edu>
2020-02-22 15:04:51 -0500

    Updates

---
+++ precache-manifest.1e7f019f96089f504a8a22fd1faef3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c151ed965296378995a6269bfd5b5957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a54b73cc4769c71b26",
+    "url": "/static/css/main.53371212.chunk.css"
+  },
+  {
+    "revision": "59a5273d3160e158eba4",
+    "url": "/static/js/2.2653f38e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2653f38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a54b73cc4769c71b26",
+    "url": "/static/js/main.60732cae.chunk.js"
+  },
+  {
+    "revision": "7dfc6d62ffcf298643f1",
+    "url": "/static/js/runtime-main.fc44378d.js"
+  }
+]);
