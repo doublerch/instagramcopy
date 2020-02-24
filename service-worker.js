c85e1d050d8cabac29639c98547e34ca1ce67a3c
Ryan Chen <chencnk@bc.edu>
2020-02-24 17:37:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e86fa01ec20fa9618d597bc0f0a94f.js"
+  "/precache-manifest.8b1038a58d0e05360b3fd0d6fae44ef3.js"
 );
 
 self.addEventListener('message', (event) => {
